fc65c951be5519fc78f92ec47693e7e162e60d32
AimilianosKass <aimilianoskas@gmail.com>
2019-04-19 14:31:53 +0300

    Close server button is now functional

--- app/src/main/java/com/example/pcremcont/activities/SettingsActivity.java
+++ app/src/main/java/com/example/pcremcont/activities/SettingsActivity.java
@@ -4,13 +4,16 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 import com.example.pcremcont.R;
+import com.example.pcremcont.portCommunicator.SendToServer;
 
 public class SettingsActivity extends AppCompatActivity
 {
     ImageButton returnBtn;
+    Button closeServerBtn;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState)
@@ -19,8 +22,17 @@ public class SettingsActivity extends AppCompatActivity
         setContentView(R.layout.settings_activity);
 
         returnBtn  = findViewById(R.id.setting_return_button);
+        closeServerBtn = findViewById(R.id.close_server_btn);
 
 
+        closeServerBtn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                sendMessage("0@");
+            }
+        });
 
         returnBtn.setOnClickListener(new View.OnClickListener()
         {
@@ -33,5 +45,10 @@ public class SettingsActivity extends AppCompatActivity
     }
 
 
+    private void sendMessage(String msg)
+    {
+        SendToServer messageSender = new SendToServer();
+        messageSender.execute(msg);
+    }
 
 }
